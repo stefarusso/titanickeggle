15be9f91005290712a41ff7f9bdffb1456cdbc56
stefarusso <rush.steve95@gmail.com>
2020-10-13 02:38:03 +0200

    plot add

--- titanic.R
+++ titanic.R
@@ -7,9 +7,20 @@ train$Pclass = as.factor(train$Pclass)
 test$Pclass = as.factor(test$Pclass)
 #write.table(head(test),sep = " | ")
 library(ggplot2)
+library(RColorBrewer)
+#display.brewer.all()
+
 #plot = ggplot(train)+
 #+ geom_bar(mapping=aes(x=Pclass,fill=Survived))
 # png("pclass.png")
 # print(plot)
 # dev.off()
-
+#tmp = train[c("Survived","Pclass")]
+#rates = table(tmp)[2,]/(colSums(table(tmp)))
+#rates = data.frame("pclass" = names(rates),"survival_rates"=unname(rates))
+#ggplot(rates,aes(x=pclass,y=surv_rates))+
+#+ geom_bar(stat="identity",fill=rev(brewer.pal(3,"Blues")))
+# png("pclass_surv.png")
+# print(plot)
+# dev.off()
+#rm(rates,tmp,plot)
